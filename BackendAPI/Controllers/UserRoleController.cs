76605194fce93380ec07c21487affed210b370f9
TalasCode <talajezzini2004@gmail.com>
2024-08-22 13:55:07 +0300

    push

---
+++ BackendAPI/Controllers/UserRoleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BackendAPI.Controllers
+{
+    public class UserRoleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
